d6d1cf3930e158aa45b58121d6e60174ba08788f
Jan Brohl <janbrohl@t-online.de>
2015-10-05 13:21:39 +0200

    Bump version to 6.0

--- lib/simpletal/__init__.py
+++ lib/simpletal/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: iso-8859-1 -*-
 import sys
-__version__ = "4.3"
+__version__ = "6.0"
 __all__ = ["simpleTAL", "simpleTALES", "simpleTALUtils"]
 if sys.version_info.major == 2:
     __all__.append("simpleElementTree")
